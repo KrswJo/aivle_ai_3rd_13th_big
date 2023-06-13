a49c370a82da6b8ebdc1c8fc59fee2e6b4dbb2f3
dhshin1125 <dhshin1125@gmail.com>
2023-06-13 15:34:26 +0900

    fix mainpage image

--- main/models.py
+++ main/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    profile = models.ImageField()
+    profile = models.ImageField(upload_to='static/img/')
     client_name = models.CharField(max_length=10)
     real_name = models.CharField(max_length=10)
