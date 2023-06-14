66b3d486ab276669a3f46c9cc02bd4a11eacec68
fourevere <jgk8866@naver.com>
2023-06-14 10:36:04 +0900

    board views function add

--- board/models.py
+++ board/models.py
@@ -4,7 +4,12 @@ class Board(models.Model):
     title       = models.CharField(max_length=200, verbose_name="제목")
     contents    = models.TextField(verbose_name="내용")
     author      = models.CharField(max_length = 100, verbose_name='작성자')
+    hit         = models.PositiveIntegerField(default = 0)
     published_date  = models.DateTimeField(auto_now=True, verbose_name="최종수정일")
 
     def __str__(self):
         return self.title
+    
+    def update_counter(self):
+        self.hit = self.hit + 1
+        self.save()
