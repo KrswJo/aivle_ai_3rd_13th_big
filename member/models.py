4c062865ea48ca00037ec859e8e48628736a9452
dhshin1125 <dhshin1125@gmail.com>
2023-06-15 13:26:46 +0900

    fix nickname null=True

--- member/models.py
+++ member/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=15, unique=True, null=True, default='admin')
+    nickname = models.CharField(max_length=15, unique=True, null=True)
     # country = models.CharField(max_length=100, unique=False, null=True)
