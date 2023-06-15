fa12f023b88ed7319f3c3bd4c29a89b171d0cdd0
dhshin1125 <dhshin1125@gmail.com>
2023-06-15 13:22:24 +0900

    fix admin nickname is admin

--- member/models.py
+++ member/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=15, unique=True, null=True)
+    nickname = models.CharField(max_length=15, unique=True, null=True, default='admin')
     # country = models.CharField(max_length=100, unique=False, null=True)
