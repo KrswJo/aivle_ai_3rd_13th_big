e6622cb3d812e612b66f6a628c809bfbce6a64d9
2017132035 <dlsghks1015@naver.com>
2023-06-15 16:26:48 +0900

    payment

--- django_for_front/urls.py
+++ django_for_front/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('main.urls')),
     path('member/', include('member.urls')),
-    path('board/', include('board.urls'))
+    path('board/', include('board.urls')),
+    path('payment/', include('payment.urls')),
 ]
