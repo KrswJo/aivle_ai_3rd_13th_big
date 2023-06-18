d3ea6dd9f9de1b27757ad5ad985cbc7203125b90
dhshin1125 <dhshin1125@gmail.com>
2023-06-18 20:20:07 +0900

    board new write & test implementation

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_main_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
