d4a50a9944731d3b02458873be02abcfb8cd163b
fourevere <jgk8866@naver.com>
2023-06-19 11:15:42 +0900

    comment delete warning, main board top 5 fixed

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# 현재 안쓰는 모델인듯하니 나중에 다시 확인하고 삭제
 class News(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import News
+from board.models import Board
 
-# Create your views here.
 class NewsList(ListView):
     model = News
     ordering = '-pk'
+    template_name = 'main/news_list.html'
+
+    def get(self, request, *args, **kwargs):
+        board = Board.objects.all().order_by('-hit', '-created_date')
+        return render(request, self.template_name, {'board_list': board})
