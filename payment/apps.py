110b496f50696918633f32ee429ac25e75813ead
2017132035JIH <dlsghks1015@naver.com>
2023-06-21 16:29:44 +0900

    savedir_change_midia

--- payment/apps.py
+++ payment/apps.py
@@ -6,10 +6,11 @@ import subprocess
 def object_detection(request,file):
     
     image = Image.open(file)
-    image.save("./yolov5/data/images/Products.jpg","JPEG")
+    # image.save("./yolov5/data/images/Products.jpg","JPEG")
+    image.save("./media/Products.jpg","JPEG")
 
     # cmd = "python C:/Users/Jung/Desktop/aivle_ai_3rd_13th_big/yolov5/detect.py --weights C:/Users/Jung/Desktop/aivle_ai_3rd_13th_big/yolov5/pretrained_weights/yolov5m_costco10.pt --img 640 --conf 0.25 --img 640 --conf 0.25 --source C:/Users/Jung/Desktop/aivle_ai_3rd_13th_big/yolov5/data/images/test3.jpg"
-    cmd = "python C:/Users/Jung/Desktop/aivle_ai_3rd_13th_big/yolov5/detect.py --weights C:/Users/Jung/Desktop/aivle_ai_3rd_13th_big/yolov5/pretrained_weights/yolov5m_costco10.pt --img 640 --conf 0.25 --img 640 --conf 0.25 --source C:/Users/Jung/Desktop/aivle_ai_3rd_13th_big/yolov5/data/images/Products.jpg"
+    cmd = "python C:/Users/Jung/Desktop/aivle_ai_3rd_13th_big/yolov5/detect.py --weights C:/Users/Jung/Desktop/aivle_ai_3rd_13th_big/yolov5/pretrained_weights/yolov5m_costco10.pt --img 640 --conf 0.25 --img 640 --conf 0.25 --source C:/Users/Jung/Desktop/aivle_ai_3rd_13th_big/media/Products.jpg"
     subprocess.run(cmd, shell=True)
 
     # context = {'image': image}
