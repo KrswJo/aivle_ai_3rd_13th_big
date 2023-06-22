05fd330782f3c71c7a537a2a55c3f8b5259f0d29
jisoleil <janggh1012@naver.com>
2023-06-22 16:48:57 +0900

    mypage.html

--- member/urls.py
+++ member/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-
+from . import views
 
 urlpatterns = [
     path('', include('allauth.urls')),
+    path('mypage/', views.mypage, name='mypage'),
 ]
--- member/views.py
+++ member/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.db import connection
+from .models import User
 
 # Create your views here.
+def mypage(request):
+        # member_user 테이블의 레코드를 가져와서 객체로 다룹니다.
+    current_user = User.objects.get(pk = request.user.pk) #username: janggh1012
+    
+    
+    
+    
+    
+    
+    context = {'current_user': current_user}
+
+    return render(request, 'account/mypage.html',context)
