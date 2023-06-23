1f39d1fcabac9d79ae5e5aab0f72bb3c1cba4f32
2017132035 <dlsghks1015@naver.com>
2023-06-23 13:37:00 +0900

    Add additional fields to User model for registration

--- member/admin.py
+++ member/admin.py
@@ -5,4 +5,4 @@ from .models import User
 from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(User, UserAdmin)
-UserAdmin.fieldsets += (("Custom fields", {"fields": ["nickname"]}),)
+UserAdmin.fieldsets += (("Custom fields", {"fields": ["nickname","costco_id","english_name","card_validity_period"]}),)
