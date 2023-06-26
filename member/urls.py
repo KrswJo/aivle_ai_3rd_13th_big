4de6cac9ec6e1ed0d3ccb33b58a93c12669b8cde
fourevere <jgk8866@naver.com>
2023-06-26 10:28:55 +0900

    member bug fixed

--- member/urls.py
+++ member/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from . import views
 
-app_name = 'member'
 urlpatterns = [
     path('', include('allauth.urls')),
     path('mypage/', views.mypage, name='mypage'),
