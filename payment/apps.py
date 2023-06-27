748c3a876ffbcb6dba4d37a0ee3a72893c78849f
dhshin1125 <dhshin1125@gmail.com>
2023-06-27 17:20:26 +0900

    for windows & linux execute

--- payment/apps.py
+++ payment/apps.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from PIL import Image
 import subprocess
 from pathlib import Path
+import platform
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +16,10 @@ def object_detection(request,file):
     image.convert('RGB')
     image.save("./detect_products/Products.jpg","JPEG")
 
-    cmd = "python " + STR_BASE_DIR + "/yolov5/detect.py --weights " + STR_BASE_DIR + "/yolov5/pretrained_weights/costco_30_best.pt --img 640 --conf 0.25 --img 640 --conf 0.25 --source " + STR_BASE_DIR + "/detect_products/Products.jpg --save-txt"
+    if platform.system() == "Windows":
+        cmd = "python " + STR_BASE_DIR + "/yolov5/detect.py --weights " + STR_BASE_DIR + "/yolov5/pretrained_weights/costco_30_best.pt --img 640 --conf 0.25 --img 640 --conf 0.25 --source " + STR_BASE_DIR + "/detect_products/Products.jpg --save-txt"
+    else:
+        cmd = "python3 " + STR_BASE_DIR + "/yolov5/detect.py --weights " + STR_BASE_DIR + "/yolov5/pretrained_weights/costco_30_best.pt --img 640 --conf 0.25 --img 640 --conf 0.25 --source " + STR_BASE_DIR + "/detect_products/Products.jpg --save-txt"
     subprocess.run(cmd, shell=True)
 
 
